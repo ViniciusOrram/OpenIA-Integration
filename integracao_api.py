8fb0df799059ea82a45524dde0a1be914ce64ee2
Vinicius <vinicius.costa@orram.com.br>
2025-02-11 11:44:37 -0300

    feat: Prompt Engineering

--- integracao_api.py
+++ integracao_api.py
@@ -5,19 +5,37 @@ import os
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+prompt_sistema = """
+Você é um categorizador de produtos.
+Você deve escolher uma categoria da lista abaixo:
+#### Lista de categorias válidas
+Beleza
+Entretenimento
+Esportes
+Outros
+#### Exemplo
+Bola de tênis
+Esportes
+"""
+
 resposta = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-16k",
     messages = [
       {
        "role" : "system",
-       "content" : "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
+       "content" : prompt_sistema
       },
       {
        "role" : "user",
-       "content" : "Gere 5 produtos"
+       "content" : "Bola de tênis de mesa"
       }
-    ]
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
 )
-
-print(resposta.choices[0].message.content)
+for i in range(0, 5):
+  print(resposta.choices[i].message.content)
 
