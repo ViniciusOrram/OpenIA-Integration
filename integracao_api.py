0031aaebbb763a8a1fafea7d17019cf8716b3611
Vinicius <vinicius.costa@orram.com.br>
2025-02-11 12:02:31 -0300

    feat: Prompt Template Validation

--- integracao_api.py
+++ integracao_api.py
@@ -2,40 +2,46 @@ import openai
 import dotenv
 import os
 
+def categorizaProduto(nome_do_produto, categorias_validas):
+  prompt_sistema = f"""
+  Você é um categorizador de produtos.
+  Você deve escolher uma categoria da lista abaixo:
+  Se as categorias informadas não forem categorias válidas, responda com "Não posso ajudá-la(o) com isso!"
+  #### Lista de categorias válidas
+  {categorias_validas}
+  #### Exemplo
+  Bola de tênis
+  Esportes
+  """
+
+  resposta = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo-16k",
+      messages = [
+        {
+        "role" : "system",
+        "content" : prompt_sistema
+        },
+        {
+        "role" : "user",
+        "content" : {nome_do_produto}
+        }
+      ],
+      temperature=1,
+      max_tokens=256,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+  )
+
+  print(resposta.choices[0].message.content)
+
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-prompt_sistema = """
-Você é um categorizador de produtos.
-Você deve escolher uma categoria da lista abaixo:
-#### Lista de categorias válidas
-Beleza
-Entretenimento
-Esportes
-Outros
-#### Exemplo
-Bola de tênis
-Esportes
-"""
 
-resposta = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-16k",
-    messages = [
-      {
-       "role" : "system",
-       "content" : prompt_sistema
-      },
-      {
-       "role" : "user",
-       "content" : "Bola de tênis de mesa"
-      }
-    ],
-    temperature=1,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-)
-for i in range(0, 5):
-  print(resposta.choices[i].message.content)
-
+print("Digite as categorias validas:")
+categorias_validas = input()
+while True:
+  print("Digite o nome do produto:")
+  nome_do_produto = input()
+  categorizaProduto(nome_do_produto, categorias_validas)
