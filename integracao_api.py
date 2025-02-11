6d70d40379fbba67a11115e833878fc0ff5971ce
Vinicius <vinicius.costa@orram.com.br>
2025-02-11 11:27:45 -0300

    Feat: Playground

---
+++ integracao_api.py
@@ -0,0 +1,23 @@
+import openai
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+resposta = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k",
+    messages = [
+      {
+       "role" : "system",
+       "content" : "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
+      },
+      {
+       "role" : "user",
+       "content" : "Gere 5 produtos"
+      }
+    ]
+)
+
+print(resposta.choices[0].message.content)
+
